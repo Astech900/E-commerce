d15f44739219d0cb5606eb7d8512f5a776b8beff
AsTech-900 <sallu@gmail.com>
2025-01-09 11:45:13 +0600

    added new Feature

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 let displayQoute = document.getElementById("quote");
 let displayAuthor= document.getElementById("author");
 let getNewQuotes = document.getElementById("new-qoutes");
+let tweetButton = document.getElementById("tweet-btn");
 
 
 getNewQuotes.addEventListener("click",()=>{
     getQuotes();
 })
 
+tweetButton.addEventListener("click",()=>{
+    window.open("https://twitter.com/intent/tweet?text=" + displayQoute.innerHTML,"Tweet Window", width="600px", height="300px")
+})
+
 let apiUrl = 'https://api.api-ninjas.com/v1/quotes';
 let apiKey = 'JdfbjWykkPShxFY7aUVhVw==cGmMnlnP1Tfu16CK';
 
 async function getQuotes() {
+
+
     fetch(apiUrl,{
         method:'GET',
         headers:{
